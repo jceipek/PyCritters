cf1002698595da6d10092cfeda726b2400023c32
Chase Kernan <chase.kernan@students.olin.edu>
2011-10-29 14:16:34 -0400

    Starting testing

--- critters/neural.py
+++ critters/neural.py
@@ -109,9 +109,13 @@ class Node(object):
     def process(self, inputs, dt):
         pass
     
+    def processReturn(self, inputs, dt):
+        self.process(inputs, dt)
+        return self.output
+    
 class InputNode(Node):
     
-    numInputs = 0
+    numInputs = 1
     
     def process(self, inputs, dt):
         self.output = inputs[0]
@@ -162,9 +166,9 @@ class SignOfNode(Node):
         self.output = utils.sign(inputs[0])
 
 
-
-layers = [[SumNode() for _ in range(3)] for _ in range(3)]
-nn = makeNeuralNetwork(5, layers)
-nn.randomizeWeights()
-inputs = range(5)
-print(nn.clone().process(inputs))
+if __name__ == '__main__':
+    layers = [[SumNode() for _ in range(3)] for _ in range(3)]
+    nn = makeNeuralNetwork(5, layers)
+    nn.randomizeWeights()
+    inputs = range(5)
+    print(nn.clone().process(inputs))
