8d84315f239c6a635e7dcb15b77569cedf16c8f4
Julian Ceipek <julian.ceipek@gmail.com>
2011-11-02 14:52:37 -0400

    Added a testing environment called visSimTest. Currently objects are only drawn using wireframe debug mode. Solid bodies are currently not rendered.

---
+++ critters/visSimTest.py
@@ -0,0 +1,51 @@
+import time
+
+import visualization.render
+from visualization.renderable import makeRenderable
+import physics.objects as objects
+
+from bullet.bullet import DiscreteDynamicsWorld, Vector3
+
+import pygame
+
+def step(world):
+    timeStep = fixedTimeStep = 1.0 / 60.0
+    world.stepSimulation(timeStep, 1, fixedTimeStep)
+    now = time.time()
+    delay = now % timeStep
+    time.sleep(delay)
+    
+ents = set()
+dynamicsWorld = DiscreteDynamicsWorld()
+
+ground = objects.StaticPlane(Vector3(0,1,0), 0.0) #Y is up
+dynamicsWorld.addRigidBody(ground.body)
+for i in range(10):
+    box = objects.Box(Vector3(0, 0.5+5*i, 0), Vector3(5.0,5.0,5.0))
+    rBox = makeRenderable(box, (255,0,0))
+    ents.add(rBox)
+    dynamicsWorld.addRigidBody(box.body)
+
+r = visualization.render.Renderer(dynamicsWorld, debug=True)
+r.setup()
+
+running = True
+rot = 0.0
+
+while running:
+    step(dynamicsWorld)
+    
+    r.render(ents)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+            elif event.key == pygame.K_LEFT:
+                rot -= 0.1
+            elif event.key == pygame.K_RIGHT:
+                rot += 0.1
+                
+                
