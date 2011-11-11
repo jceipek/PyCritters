6ab1ce0cac8576e0b4f63e0850e6774e94f533f8
Chase Kernan <chase.kernan@students.olin.edu>
2011-11-11 14:44:57 -0500

    Added expand morphology

--- critters/morph.py
+++ critters/morph.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import neural
 import math
+import copy
 
 from random import random
 
@@ -20,7 +21,61 @@ class Morphology(object):
     def createConnection(self, *nodes): 
         #XXX Do you really want new nodes to be created every time
         #we add a connection? - Julian
+        # They aren't... the graph.add_node function does nothing if the node 
+        # is already in the graph. Its just a safe call to catch a case where
+        # a connection is added between nodes not already in the graph.
+        # Maybe change this to an error? - Chase
         self.addConnection(MorphConnection(tuple(nodes)))
+        
+    @property
+    def nodes(self): 
+        return self.graph.nodes_iter()
+    
+    @property
+    def connections(self):
+        for _, _, data in self.graph.edges_iter(data=True):
+            yield data['connection']
+            
+    def removeNode(self, node):
+        """Removes a node and its connections from the morphology
+        """
+        self.graph.remove_node(node)
+        
+    def clone(self):
+        newNN = copy.deepcopy(self)
+        return newNN
+    
+    def expand(self):
+        cache = {}
+        def expandNode(node, depth):
+            key = node, depth
+            try:
+                return copy.deepcopy(cache[key])
+            except KeyError: pass
+            
+            graph = nx.Graph()
+            root = copy.deepcopy(node)
+            graph.add_node(root)
+            
+            for _, neighbor, data in self.graph.out_edges_iter(data=True):
+                connection = data['connection']
+                
+                if node is neighbor:
+                    subDepth = depth + 1
+                    if connection.recursionLimit >= subDepth: continue
+                else:
+                    subDepth = 0
+                
+                subroot, subgraph = expandNode(neighbor, subDepth)
+                
+                graph.add_nodes_from(subgraph.nodes_iter())
+                graph.add_edges_from(subgraph.edges_iter(data=True))
+                
+                graph.add_edge(root, subroot, { 'connection': connection })
+            
+            cache[key] = root, graph
+        
+        return expandNode(next(self.nodes), 0)[0]
 
 class MorphNode(object):
     
