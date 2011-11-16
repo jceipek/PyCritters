7205e1f3c50dbf634661a29e1ae03351e7b8607c
wcdolphin <wcdolphin@gmail.com>
2011-11-15 18:17:04 -0800

    added comments to simplified hinge Demo

--- demos/hingeDemoSimplified.py
+++ demos/hingeDemoSimplified.py
@@ -9,32 +9,29 @@ from bullet.bullet import Vector3
 
 simEnv= SimulationEnvironment()
 
-
 box1 = Box(Vector3(0, 2.5, 0), Vector3(9.0,5.0,5.0))
-
 box2 = Box(Vector3(9.0, 2.5, 0), Vector3(9.0,5.0,5.0))
-
 
 simEnv.addPhysicsObject(box1)
 simEnv.addPhysicsObject(box2)
 
 simEnv.addHinge(box1, box2, Vector3(4.5,0,0),Vector3(0,0,1),Vector3(0,1,0))
 
-motors = [simEnv.getConstraint(box1,box2).getRotationalLimitMotor(2)]
 hinge =simEnv.getConstraint(box1,box2)
+motor = hinge.getRotationalLimitMotor(2) #get the Z rotational motor
+
+#Constrain all axis of rotation other than the Z to a maximum and minimum of 0 
 hinge.getRotationalLimitMotor(0).hiLimit = 0
 hinge.getRotationalLimitMotor(0).loLimit = 0
 hinge.getRotationalLimitMotor(1).hiLimit = 0
 hinge.getRotationalLimitMotor(1).loLimit = 0
 
-for motor in motors:
-    motor.enableMotor = True
-    motor.targetVelocity = 10
-    motor.hiLimit = 90
-    motor.loLimit = -90
-    motor.maxMotorForce = 50
-    print(motor.maxMotorForce)
-    #print(motor.currentPosition)
+#enable Z axis motor, configure limits of the hinge and set a maxMotorForce
+motor.enableMotor = True
+motor.targetVelocity = 10
+motor.hiLimit = 90
+motor.loLimit = -90
+motor.maxMotorForce = 50
 
 
 
