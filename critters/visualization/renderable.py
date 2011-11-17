6a1e81ab5794e315bf467eea0c8967f919499cc5
Julian Ceipek <julian.ceipek@gmail.com>
2011-11-16 20:36:38 -0500

    Potentially drawing connections now, but they look very wrong.

--- critters/visualization/renderable.py
+++ critters/visualization/renderable.py
@@ -16,19 +16,21 @@ class RenderableBox(Renderable):
     pass
     
 class RenderableConnection(Renderable):
-    def __init__(self, po1, po2, local1, local2, color):
-        if color == None: color = (1.0, 0.0, 0.0)
-        Renderable.__init__(self, hinge, color)
+    def __init__(self, obj, color):
+        self.po1, self.po2, self.local1, self.local2 = obj
+        if color == None:
+            color = (1.0, 0.0, 0.0)
+        Renderable.__init__(self, None, color)
 
     def render(self):
         glBegin(GL_LINES)
-        v1 = po1.body.getWorldTransform().getOrigin() - local1
-        v2 = po2.body.getWorldTransform().getOrigin() - local2
+        v1 = self.po1.body.getWorldTransform().getOrigin() - self.local1
+        v2 = self.po2.body.getWorldTransform().getOrigin() - self.local2
         glVertex(*v1)
         glVertex(*v2)
         glEnd()
 
 def makeRenderable(obj, color=None):
     if(isinstance(obj, tuple)):
-        return RenderableConnection(*obj, color)
+        return RenderableConnection(obj, color)
     return Renderable(obj, color)
