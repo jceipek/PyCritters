e0ea9f256adeee095125c21801ae31b397157d44
Cory Dolphin <wcdolphin@gmail.com>
2011-11-29 14:15:49 -0500

    removed old code fro simEnv

--- demos/simEnvDemo.py
+++ demos/simEnvDemo.py
@@ -9,12 +9,6 @@ sys.path.append('../critters')  #add critters to pythonpath to use as library
 from physics.simulationEnvironment import SimulationEnvironment
 from physics import objects 
 
-# --- constants ---
-# Box2D deals with meters, but we want to display pixels, 
-# so define a conversion factor:
-TARGET_FPS=60
-TIME_STEP=1.0/TARGET_FPS
-SCREEN_WIDTH, SCREEN_HEIGHT=640,480
 
 simEnv = SimulationEnvironment(vis=True, gravity=True)
 
