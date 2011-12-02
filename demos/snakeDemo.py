bafe4d8fa9eb0f2efb5d817bc7c07c15c6dd9753
wcdolphin <wcdolphin@gmail.com>
2011-12-01 21:08:22 -0500

    added snakeDemo.py

---
+++ demos/snakeDemo.py
@@ -0,0 +1,29 @@
+"""
+"""
+
+from critters.physics import simulationEnvironment
+from critters.physics import objects
+
+from critters import (neural,life,morph)
+
+#physics.objects.Rect
+#critters.objects.Rect
+
+
+morphology = morph.createInchWorm()
+net = neural.simpleSineNetwork(1, 1)
+creature = life.Critter(morphology, net, 1)
+
+simEnv = simulationEnvironment.SimulationEnvironment(vis=True, gravity=False)
+
+rects,hinges = simEnv.addCreature(creature)
+
+print rects
+print hinges
+
+joint = simEnv.getConstraint(rects[0], rects[1])
+print joint.motorSpeed
+joint.motorSpeed = 2
+#simEnv.addPhysicsObject(ground)
+
+simEnv.run(offset=(500,-300))
