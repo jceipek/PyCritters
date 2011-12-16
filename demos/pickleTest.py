5ecefea90642adaf38f62ea95e56c44d81dcdcb8
Chase Kernan <chase.kernan@students.olin.edu>
2011-12-15 23:11:31 -0500

    Cdf generation on pickling

--- demos/pickleTest.py
+++ demos/pickleTest.py
@@ -9,19 +9,57 @@ from critters.physics.simulationEnvironment import SimulationEnvironment
 from critters.life import Critter
 import datetime
 import pickle
+from critters import Cdf
+from matplotlib import pyplot
 
 
 def main(main,fil='/home/wdolphin/PyCritters/critters/output/max_4'):
-
     myCrit = pickle.load(open(fil ,'r'))
     
+    cdf = generateCdf(myCrit)
+    pyplot.plot(*cdf.Render())
     
-    simEnv = SimulationEnvironment()
+    print "SCORE: %.2f" % _doCalculation(myCrit)
     
-    simEnv.addCreature(myCrit)
     
-    simEnv.simulate(offset=(500,-300)) #offset is for initial window placement
+def generateCdf(c, n=20):
+    return Cdf.MakeCdfFromList(_doCalculation(c, False) for _ in range(n))
 
+def _doCalculation(individual, vis=True):       
+    simEnv = SimulationEnvironment(vis=vis)
+    failure = individual, 1e-10
+    
+    try:
+        body = simEnv.addCreature(individual)
+    except RuntimeError as e:
+        print e
+        return failure
+    
+    if not body: return failure
+    
+    before = simEnv.getMeanX(body)
+    
+    def calculateScore():
+        score = simEnv.getMeanX(body) - before
+        #print "Score %.2f, Before %.2f, MeanX %.2f" % \
+        #     (score, before, simEnv.getMeanX(body))
+        
+        rects, _ = body
+        if len(rects) > 8:
+            score /= len(rects) - 8
+        if simEnv.isBelowGround(body):
+            score = 1e-10
+        
+        #print "Final score: %.2f " % max(self.MIN_FITNESS, score)
+        return max(1e-10, score)
+    
+    def check():
+        return calculateScore() >= 4.0
+    
+    simEnv.simulate(timeToRun=50.0, check=(20.0, check))
+    
+    finalScore = calculateScore()
+    return individual, finalScore
 
 if __name__ == '__main__':
     import sys
